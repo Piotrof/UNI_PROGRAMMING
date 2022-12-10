27884adee584d73542a47ec9288a104d07ef0cb2
Piotrof <piotrowicz.jakub.e@gmail.com>
2022-12-10 11:50:59 +0100

    Added methods to the Ring class
    added methods:
    -print,
    -add,
    -split.

--- ADS/lab_2/Ring/Ring/Ring.h
+++ ADS/lab_2/Ring/Ring/Ring.h
@@ -20,17 +20,53 @@ private:
 		return new_node;
 	};
 
-	Node head;
+	Node* head;
+
 public:
 	Ring() = default;
 	Ring(Ring& other_obj);
 	~Ring() = default;
+
+	bool add(Key key);
+
+	void print();
 };
 
 //class methods for the Ring class
 template<typename Key>
 Ring<Key>::Ring(Ring& other_obj) {
 
+	Node* curr = this->head;
+
+
+}
+
+template<typename Key>
+bool Ring<Key>::add(Key key) {
+
+	Node* curr = this->head->prev;
+	curr->next = allocate_node(key);
+	Node* temp = curr;
+	curr = curr->next;
+	curr->next = this->head;
+	curr->prev = temp;
+	this->head->prev = curr;
+}
+
+template<typename Key>
+void Ring<Key>::print() {
+
+	if (!this->head) return;
+
+	Node* curr = this->head;
+	int i = 0;
+
+	cout << "Printing Ring.\n";
+	do {
+		cout << "El. no. " << i << ": " << curr->key << ".\n";
+
+		curr = curr->next;
+	} while (curr->next != this->head);
 }
 
 //external split function
